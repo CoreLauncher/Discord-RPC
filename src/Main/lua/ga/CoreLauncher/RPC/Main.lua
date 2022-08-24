d08a259fe86e6760588dcf58647386eb3f0629ea
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-24 08:22:19 +0200

    change start stuff

--- src/Main/lua/ga/CoreLauncher/RPC/Main.lua
+++ src/Main/lua/ga/CoreLauncher/RPC/Main.lua
@@ -1,39 +1,53 @@
 local FS = require("fs")
+local Request = require("coro-http").request
+
 local ApplicationData = TypeWriter.ApplicationData .. "/Discord-RPC/"
 FS.mkdirSync(ApplicationData)
+
 local GetNodeFile = ApplicationData .. "Get-Node.twr"
-local Success = pcall(
+local OpenIPCFile = ApplicationData .. "Open-IPC.twr"
+
+local Success, Error = pcall(
     function ()
-        local Response, Body = require("coro-http").request(
+        local _, GetNode = Request(
             "GET",
             "https://github.com/corebytee/get-node/releases/latest/download/Get-Node.twr"
         )
-        FS.writeFileSync(GetNodeFile, Body)
+        FS.writeFileSync(GetNodeFile, GetNode)
+
+        local _, OpenIPC = Request(
+            "GET",
+            "https://github.com/CoreBytee/open-ipc/releases/latest/download/IPC-Bootstrap.twr"
+        )
+        FS.writeFileSync(OpenIPCFile, OpenIPC)
     end
 )
-if FS.existsSync(GetNodeFile) == false then
-    error("Failed to download Get-Node.twr")
+if Success == false then
+    error("Failed to download dependencies " .. Error)
 end
+
 TypeWriter.Runtime.LoadInternal("BetterEmitter")
 TypeWriter.Runtime.LoadFile(GetNodeFile)
+TypeWriter.Runtime.LoadFile(OpenIPCFile)
 local NodePath = Import("ga.corebyte.get-node").Download()
+local IPClient = Import("openipc.bootstrap").LoadAll()
 
+local ExtractedVersion = FS.readFileSync(ApplicationData .. "version.txt")
+local CurrentVersion = TypeWriter.LoadedPackages["Discord-RPC"].Package.Version
+p(CurrentVersion)
 
-local InstalledPackageJson = FS.readFileSync(ApplicationData .. "/package.json")
-local PackageJson = TypeWriter.LoadedPackages["Discord-RPC"].Resources["/package.json"]
-FS.writeFileSync(ApplicationData .. "/package.json", PackageJson)
-if InstalledPackageJson ~= PackageJson then
-    require("coro-fs").rmrf(ApplicationData .. "/node_modules")
-    local Result = require("coro-spawn")(
-        NodePath .. "/npm" .. (({["win32"] = ".cmd"})[TypeWriter.Os] or ""),
-        {
-            args = {
-                "i"
-            },
-            cwd = ApplicationData
-        }
-    )
-    Result.waitExit()
+if CurrentVersion ~= ExtractedVersion then
+    require("coro-fs").rmrf(ApplicationData .. "/Node/")
+    local Files = {
+        "index.js",
+        "OpenIPC-NodeJs.zip",
+        "package.json",
+    }
+    FS.mkdirSync(ApplicationData .. "/Node/")
+    for Index, FilePath in pairs(Files) do
+        local FileData = TypeWriter.LoadedPackages["Discord-RPC"].Resources["/Node/" .. FilePath]
+        FS.writeFileSync(ApplicationData .. "/Node/" .. FilePath, FileData)
+    end
+    FS.writeFileSync(ApplicationData .. "version.txt", CurrentVersion)
+    Import("ga.CoreLauncher.RPC.Lib.Unzip")(ApplicationData .. "/Node/OpenIPC-NodeJs.zip", ApplicationData .. "/Node/OpenIPC")
 end
-
-return Import("ga.CoreLauncher.RPC.RPC")(NodePath, ApplicationData)
