04b45481ad3c6806d36a5af4fee58cc5cb632654
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-24 08:22:23 +0200

    Update RPC.lua

--- src/Main/lua/ga/CoreLauncher/RPC/RPC.lua
+++ src/Main/lua/ga/CoreLauncher/RPC/RPC.lua
@@ -6,64 +6,15 @@ return function (NodePath, ApplicationData)
     local RPC = Emitter:extend()
 
     function RPC:initialize(ClientId)
-        self.Process, Error = Spawn(
-            NodePath .. "/node",
-            {
-                args = {
-                    "-i"
-                },
-                cwd = ApplicationData
-            }
-        )
-        p(Error)
-
-        coroutine.wrap(
-            function ()
-                for Message in self.Process.stdout.read do
-                    p(Message)
-                    self:Emit("stdout", Message)
-                end
-            end
-        )()
-
-        coroutine.wrap(
-            function ()
-                for Message in self.Process.stderr.read do
-                    print(Message)
-                end
-            end
-        )()
-
-        self:WaitForWrite("> ")
-        self:Write(string.format("var Client = new (require('@xhayper/discord-rpc').Client)({clientId: '%s'}); await Client.login()", ClientId))
-        self:WaitForWrite("undefined\n")
-    end
-
-    function RPC:Write(Text)
-        print(Text)
-        self.Process.stdin.write(Text .. "\n")
-    end
-
-    function RPC:WaitForWrite(Text)
-        p("Waiting ", Text)
-        self:WaitFor("stdout", nil, function (Message)
-            return Message == Text
-        end)
-    end
-
-    function RPC:Stop()
-        require("uv").process_kill(self.Process.handle)
+        
     end
 
     function RPC:SetActivity(Activity)
-        local S = string.random(16)
-        RPC:Write(
-            string.format(
-                "Client.user.setActivity(JSON.parse('%s'));",
-                Json.stringify(Activity)
-            )
-        )
+        
+    end
 
+    function RPC:ClearActivity()
+        
     end
 
     return RPC
