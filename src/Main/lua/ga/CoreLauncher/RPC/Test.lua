b382ae3b720cdbc8ea645674c1644be693b767dc
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-26 19:39:03 +0200

    testing

--- src/Main/lua/ga/CoreLauncher/RPC/Test.lua
+++ src/Main/lua/ga/CoreLauncher/RPC/Test.lua
@@ -1,7 +1,13 @@
 print("Hello World!")
 local RPC = Import("ga.CoreLauncher.RPC"):new("791927279357657088")
-RPC:SetActivity(
+p(RPC:SetActivity(
     {
-        state = string.random(16)
+        state = string.random(16),
+        buttons = {
+            {
+                label = "test",
+                url = "https://cubic-inc.ga"
+            }
+        }
     }
-)
+))
