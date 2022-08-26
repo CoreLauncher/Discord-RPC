f8f0adcaa173750a38a43a93730c0fdaa039c944
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-26 19:39:53 +0200

    npm install stuff

--- src/Main/lua/ga/CoreLauncher/RPC/Lib/NPM.lua
+++ src/Main/lua/ga/CoreLauncher/RPC/Lib/NPM.lua
@@ -1,19 +1,21 @@
 local Spawn = require("coro-spawn")
-return function (Path)
-    local Result = Spawn(
-        NodePath .. "/npm" .. (({["win32"] = ".cmd"})[TypeWriter.Os] or ""),
-        {
-            args = {
-                "install",
-                "--omit=dev"
-            },
-            cwd = Location,
-            stdio = {
-                process.stdin.handle,
-                process.stdout.handle,
-                process.stderr.handle,
+return function (NodePath)
+    return function (Path)
+        local Result = Spawn(
+            NodePath .. "/npm" .. (({["win32"] = ".cmd"})[TypeWriter.Os] or ""),
+            {
+                args = {
+                    "install",
+                    "--omit=dev"
+                },
+                cwd = Path,
+                stdio = {
+                    process.stdin.handle,
+                    process.stdout.handle,
+                    process.stderr.handle,
+                }
             }
-        }
-    )
-    Result.waitExit()
+        )
+        Result.waitExit()
+    end
 end
