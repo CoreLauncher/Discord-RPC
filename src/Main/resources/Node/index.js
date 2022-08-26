fb0500af6bd859c80c008ddab4327f1ce9808603
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-26 19:38:55 +0200

    js part

---
+++ src/Main/resources/Node/index.js
@@ -0,0 +1,37 @@
+(async function() {
+    const LaunchArgs = JSON.parse(Buffer.from(process.argv[2], "base64"))
+    console.log(LaunchArgs)
+    const RPC = new (require("@xhayper/discord-rpc").Client)({clientId: LaunchArgs.ClientId})
+    await RPC.login()
+    const IpcConnection = new (require("./OpenIPC/index.js"))(LaunchArgs.Channel, "Node")
+
+    IpcConnection.RegisterMessage(
+        "SetActivity",
+        async function(Activity) {
+            return await RPC.user?.setActivity(
+                Activity
+            );
+        }
+    )
+
+    IpcConnection.RegisterMessage(
+        "ClearActivity",
+        async function() {
+            return await RPC.user?.clearActivity();
+        }
+    )
+
+    IpcConnection.RegisterMessage(
+        "Disconnect",
+        async function() {
+            await RPC.destroy()
+            Disconnect()
+        }
+    )
+
+    async function Disconnect() {
+        await IpcConnection.Disconnect()
+    }
+
+    IpcConnection.Send("Main", "Connected")
+})()
